c8c3ba493795b870c986e68fbff3e37382cc3e33
BevinTheBeverage <38870147+BevinTheBeverage@users.noreply.github.com>
2022-07-14 13:29:58 -0400

    switched to grid format

--- gui.py
+++ gui.py
@@ -1,17 +1,52 @@
 from tkinter import *
 
+TOPIC_ENTRY_WIDTH = 4
+
+class Topic:
+    def __init__(self, text=None):
+        self.topic_entry = Entry(font=('Times New Roman', 16))
+        if(text):
+            self.set_text(text)
+        self.expand_btn = Button(root, text="+", font=('Times New Roman', 14), command=self.toggle_subtopics)
+
+    def toggle_subtopics(self):
+        # TODO
+        return
+
+    def set_text(self, text):
+        self.topic_entry.delete(0,END)
+        self.topic_entry.insert(0,text)
+
+    def grid(self, r, c):
+        self.topic_entry.grid(row=r, column=c, columnspan=TOPIC_ENTRY_WIDTH)
+        self.expand_btn.grid(row=r, column=c+TOPIC_ENTRY_WIDTH)
+
 # initialize tkinter
 root = Tk()
 root.title("Press Clips Reporter")
 
 title = Label(root, text='Press Clips Reporter', font=('Times New Roman', 24))
-title.pack(padx=100, pady=(25, 0))
+title.grid(row=0, column=1, ipadx=10, ipady=10)
 
-topics_label = Label(root, text='Topics', font=('Times New Roman', 20), justify=LEFT)
-topics_label.pack(padx=50, anchor='w')
+topics_label = Label(root, text='Topics', font=('Times New Roman', 20))
+topics_label.grid(row=1, column=1, ipadx=10, ipady=10)
 
-topic_entries = [Entry(font=('Times New Roman', 16))]
-for entry in topic_entries:
-    entry.pack(pady=(5, 25), anchor='w', padx=55)
+default_topics = [
+        'Yuh-Line Niou',
+        'Bill de Blasio'
+    ]
+
+topic_entries = []
+
+def add_topic(text):
+    topic_entries.append(Topic(text))
+
+for default_topic in default_topics:
+    add_topic(default_topic)
+
+for i, topic in enumerate(topic_entries):
+    topic.grid(c = 0, r = i+3)
+
+add_topic_btn = Button(root, text= "Add Topic", font=('Times New Roman', 14), command=add_topic)
 
 root.mainloop()
