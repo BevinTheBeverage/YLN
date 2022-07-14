64b22955a497b6428502ea6f75db82483b6aa2f5
BevinTheBeverage <38870147+BevinTheBeverage@users.noreply.github.com>
2022-07-14 17:58:48 -0400

    refactored hiding/showing buttons

--- gui.py
+++ gui.py
@@ -26,8 +26,7 @@ class Topic:
             for entry in self.subtopic_entries:
                 entry.grid_remove()
             self.subtopics_label.grid_remove()
-            self.add_subtopics_btn.grid_remove()
-            self.del_subtopics_btn.grid_remove()
+            self.hide_buttons()
         else:
             for topic in topic_entries:
                 if topic.subtopics_shown:
@@ -35,27 +34,30 @@ class Topic:
             for i, entry in enumerate(self.subtopic_entries):
                 entry.grid(row=self.row+i+2, column=1, padx=(60,0))
             self.subtopics_label.grid(row=self.row+1, column=self.col+1)
-            self.add_subtopics_btn.grid(row=self.row+2+MAX_SUBTOPICS, column=self.col+1, columnspan=2)
-            self.del_subtopics_btn.grid(row=self.row+3+MAX_SUBTOPICS, column=self.col+1, columnspan=2)
+            self.show_buttons()
         self.subtopics_shown = not self.subtopics_shown
+
+    def hide_buttons(self):
+        self.add_subtopics_btn.grid_remove()
+        self.del_subtopics_btn.grid_remove()
+
+    def show_buttons(self):
+        self.add_subtopics_btn.grid(row=self.row+2+MAX_SUBTOPICS, column=self.col+1, columnspan=2)
+        self.del_subtopics_btn.grid(row=self.row+3+MAX_SUBTOPICS, column=self.col+1, columnspan=2)
 
     def add_subtopic(self):
         if len(self.subtopic_entries) < MAX_SUBTOPICS:
             self.subtopic_entries.append(Entry(font=('Times New Roman', 16), width = 15))
-            self.add_subtopics_btn.grid_remove()
-            self.del_subtopics_btn.grid_remove()
+            self.hide_buttons()
             self.subtopic_entries[-1].grid(row=self.row+1+len(self.subtopic_entries), column=1, padx=(60,0))
-            self.add_subtopics_btn.grid(row=self.row+2+MAX_SUBTOPICS, column=self.col+1, columnspan=2)
-            self.del_subtopics_btn.grid(row=self.row+3+MAX_SUBTOPICS, column=self.col+1, columnspan=2)
+            self.show_buttons()
 
     def remove_subtopic(self):
         if len(self.subtopic_entries) != 0:
             entry = self.subtopic_entries.pop(-1)
             entry.grid_remove()
-            self.add_subtopics_btn.grid_remove()
-            self.del_subtopics_btn.grid_remove()
-            self.add_subtopics_btn.grid(row=self.row+2+MAX_SUBTOPICS, column=self.col+1, columnspan=2)
-            self.del_subtopics_btn.grid(row=self.row+3+MAX_SUBTOPICS, column=self.col+1, columnspan=2)
+            self.hide_buttons()
+            self.show_buttons()
 
     def set_text(self, entry, text):
         entry.delete(0,END)
@@ -70,8 +72,7 @@ class Topic:
         self.topic_entry.grid_remove()
         self.expand_btn.grid_remove()
         self.subtopics_label.grid_remove()
-        self.add_subtopics_btn.grid_remove()
-        self.del_subtopics_btn.grid_remove()
+        self.hide_buttons()
 
 # initialize tkinter
 root = Tk()
@@ -85,14 +86,17 @@ topics_label.grid(row=1, column=0, columnspan=4)
 
 topic_entries = []
 
-def add_topic(text=None, subtopics = []):
-    topic = (Topic(text, subtopics))
-    topic_entries.append(topic)
-    topic.grid(c = 0, r = (len(topic_entries)-1)*(MAX_SUBTOPICS+4)+2)
+def refresh_buttons():
     add_topic_btn.grid_remove()
     delete_topic_btn.grid_remove()
     add_topic_btn.grid(column=1, row = len(topic_entries)*(MAX_SUBTOPICS+4)+3, pady=30, columnspan=2)
     delete_topic_btn.grid(column=1, row=len(topic_entries)*(4+MAX_SUBTOPICS)+4, pady=(0, 10), columnspan=2)
+
+def add_topic(text=None, subtopics = []):
+    topic = (Topic(text, subtopics))
+    topic_entries.append(topic)
+    topic.grid(c = 0, r = (len(topic_entries)-1)*(MAX_SUBTOPICS+4)+2)
+    refresh_buttons()
 
 add_topic_btn = Button(root, text= "Add Topic", font=('Times New Roman', 14), command=add_topic)
 
@@ -100,10 +104,7 @@ def delete_topic():
     if len(topic_entries) != 0:
         topic = topic_entries.pop(-1)
         topic.delete()
-        add_topic_btn.grid_remove()
-        delete_topic_btn.grid_remove()
-        add_topic_btn.grid(column=1, row = len(topic_entries)*(MAX_SUBTOPICS+4)+3, pady=30, columnspan=2)
-        delete_topic_btn.grid(column=1, row=len(topic_entries)*(4+MAX_SUBTOPICS)+4, pady=(0, 10), columnspan=2)
+        refresh_buttons()
 
 delete_topic_btn = Button(root, text= "Remove Topic", font=('Times New Roman', 14), command=delete_topic)
 
